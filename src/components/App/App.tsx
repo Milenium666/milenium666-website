fa4da61076b08d35596134bc7cdc607a2d8fb9c3
Milenium666 <whiteangel64rus@gmail.com>
2025-12-01 20:14:01 +0300

    feat(header): finalize design and structure

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,13 @@
 import './App.css'
 
+import Header from '../Header/Header';
+
 function App() {
 
   return (
     <>
-      App works!
+
+      <Header />
     </>
   )
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import './Header.css'
+
+import logo from '../../images/logo.svg';
+
+function Header() {
+
+  return (
+   <header className='header'>
+      <img className='header__logo' alt='Логотип сайта' src={logo} />
+   </header>
+  )
+}
+
+export default Header;
